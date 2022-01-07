ad7c3f480d6649710ca3bf516815a65bd150de5f
ElliottZaki <spudnick99@hotmail.com>
2022-01-07 17:39:57 +0000

    Using sessions

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
-
 class Battle < Sinatra::Base
     configure :development do
         register Sinatra::Reloader
     end
+
+enable :sessions
 
     get '/' do
         erb(:index)
@@ -16,8 +17,14 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-        @player_1 = params[:player_1]
-        @player_2 = params[:player_2]
+        session[:player_1] = params[:player_1]
+        session[:player_2] = params[:player_2]
+        redirect '/play'
+    end
+
+    get '/play' do
+        @player_1 = session[:player_1]
+        @player_2 = session[:player_2]
         erb(:play)
     end
 
